99c723f01946981b99ba615b9d2942ef606f388d
dmitkon <dmitriy.kononec@mail.ru>
2022-03-29 01:57:27 +0300

    search_suggest_and_image_test_begin

--- test_tensor.py
+++ test_tensor.py
@@ -19,25 +19,42 @@ class YandexScenariosTest(unittest.TestCase):
     def test_yandex_search(self):
         driver = self.driver
         
-        # Поиск
-        elem = find_element_by_func(driver.find_element, By.ID, "text")
-        self.assertNotEqual(elem, None, "search not found")
+        search = find_element_by_func(driver.find_element, By.ID, "text")
+        self.assertNotEqual(search, None, "search not found")
 
-        elem.send_keys("Тензор")
-        #elem1 = driver.find_element(By.ID, "suggest-list-12ciq544100c")
+        search.send_keys("Тензор")
+        suggest = find_element_by_func(driver.find_element, By.CLASS_NAME, "mini-suggest__popup-content")
+        self.assertNotEqual(suggest, None, "suggest not found")
 
-        elem.send_keys(Keys.RETURN)
+        search.send_keys(Keys.RETURN)
         
-        # Результаты поиска
-        elem = find_element_by_func(driver.find_element, By.ID, "search-result")
-        self.assertNotEqual(elem, None, "results search not found")
+        result = find_element_by_func(driver.find_element, By.ID, "search-result")
+        self.assertNotEqual(result, None, "results search not found")
         
-        links = elem.find_elements(By.XPATH, "//a[@accesskey]")
+        links = result.find_elements(By.XPATH, "//a[@accesskey]")
         url_tensor = list(map(lambda a: a.get_attribute("href").find("tensor.ru"), links))
         self.assertTrue(sum(url_tensor[:5]) > -5, "tensor.ru not found")
 
-    #def test_yandex_image(self):
-        #pass
+    def test_yandex_image(self):
+        driver = self.driver
+
+        image_link = find_element_by_func(driver.find_element, By.LINK_TEXT, "Картинки")
+        self.assertNotEqual(image_link, None, "image link not found")
+
+        image_link.click()
+        driver.get(image_link.get_attribute("href"))
+        self.assertEqual(driver.current_url.split('?')[0], "https://yandex.ru/images/")
+
+        #group = driver.find_elements(By.XPATH, "//div[@data-grid-name='im']")
+        #print(list(map(lambda a: a.get_attribute("data-grid-text"), group)))
+        
+        group = driver.find_element(By.XPATH, "//div[@data-grid-name='im'][1]")
+        group.click()
+        search = driver.find_element(By.NAME, "text")
+        self.assertEqual(group.get_attribute("data-grid-text"), search.get_attribute("value"))
+
+        #image = driver.find_element(By.XPATH, "//div[@role='item'][1]")
+        #image.click()
 
     def tearDown(self):
         self.driver.quit()
